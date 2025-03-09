7545fed8eeca2cea807901db16523e72429ca878
viceede <d4rell1@yandex.ru>
2025-03-09 22:05:18 +0300

    перпеределанный вариант

--- Задача 6/6.py
+++ Задача 6/6.py
@@ -1,18 +1,14 @@
 import math
 
 
-def calc(delta_set):
-    answ = 0
-    for delta in delta_set:
-        answ += math.floor(delta / 5)
-    return answ
+def main(psi):
+    O = {math.floor(psi_i / 9) + psi_i ** 3 for psi_i in psi if -73 <= psi_i <= 42}
 
+    Delta = {nu + abs(nu) for nu in range(-100, 101) if nu >= -23 or nu <= -52}
 
-def main(input_set):
-    O = {math.floor(psi / 9) + psi ** 3 for psi in input_set if -73 <= psi <= 42}
+    zeta = len(O)
+    for o in O:
+        for delta in Delta:
+            zeta += 6 * o + math.floor(delta / 5)
 
-    Delta = {nu + abs(nu) for nu in range(-100, 101) if -52 <= nu <= -23}
-
-    answ = len(O) + sum(6 * o for o in O) + calc(Delta) * len(O)
-
-    return answ
+    return zeta
