50615860f13a44c8809fe1e1cf2b7494e56d253e
viceede <d4rell1@yandex.ru>
2025-03-09 22:01:44 +0300

    Ðадаптированная версия 6 задачи

--- Задача 6/6.py
+++ Задача 6/6.py
@@ -1,26 +1,18 @@
-from math import floor
+import math
 
 
-def calculate_o_set(psi):
-    return {floor(psi_i / 9) + psi_i ** 3 for psi_i in psi if -73 <= psi_i <= 42}
+def calc(delta_set):
+    answ = 0
+    for delta in delta_set:
+        answ += math.floor(delta / 5)
+    return answ
 
 
-def calculate_delta_set():
-    return {nu + abs(nu) for nu in range(-100, 100) if nu >= -23 or nu <= -52}
+def main(input_set):
+    O = {math.floor(psi / 9) + psi ** 3 for psi in input_set if -73 <= psi <= 42}
 
+    Delta = {nu + abs(nu) for nu in range(-100, 101) if -52 <= nu <= -23}
 
-def calculate_n_set(psi):
-    return {psi_i % 3 + floor(psi_i / 2) for psi_i in psi if psi_i >= 30 or psi_i <= -86}
+    answ = len(O) + sum(6 * o for o in O) + calc(Delta) * len(O)
 
-
-def main(psi):
-    o_set = calculate_o_set(psi)
-    delta_set = calculate_delta_set()
-    n_set = calculate_n_set(psi)
-
-    zeta = len(o_set)
-    for o in o_set:
-        for delta in delta_set:
-            zeta += 6 * o + floor(delta / 5)
-
-    return zeta
+    return answ
