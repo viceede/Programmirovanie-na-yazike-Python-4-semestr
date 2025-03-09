6c9509f96d13d6adcfe26a0a7f5637344c2568e5
viceede <d4rell1@yandex.ru>
2025-03-09 21:47:04 +0300

    начало ветки dev

--- Задача 6/6.py
+++ Задача 6/6.py
@@ -1,29 +1,19 @@
 from math import floor
 
 
-def calculate_o_set(psi):
-    return {floor(psi_i / 9) + psi_i ** 3 for psi_i in psi if -73 <= psi_i <= 42}
-
-
-def calculate_delta_set():
-    return {nu + abs(nu) for nu in range(-100, 100) if nu >= -23 or nu <= -52}
-
-
-def calculate_n_set(psi):
-    return {psi_i % 3 + floor(psi_i / 2) for psi_i in psi if psi_i >= 30 or psi_i <= -86}
-
-
 def main(psi):
-    o_set = calculate_o_set(psi)
-    delta_set = calculate_delta_set()
-    n_set = calculate_n_set(psi)
+    O = {floor(psi_i / 9) + psi_i ** 3 for psi_i in psi if -73 <= psi_i <= 42}
 
-    zeta = len(o_set)
-    for o in o_set:
-        for delta in delta_set:
+    Delta = {nu + abs(nu) for nu in range(-100, 101) if -52 <= nu <= -23}
+
+    N = {psi_i % 3 + floor(psi_i / 2) for psi_i in psi if psi_i >= 30 or psi_i <= -86}
+
+    zeta = len(O)
+    for o in O:
+        for delta in Delta:
             zeta += 6 * o + floor(delta / 5)
 
     return zeta
 
-x={32, -92, 72, -52, -81, 82, -45, 84, 29}
+x={-64, 65, 99, 36, -88, -21, -14, -78, -43, -2}
 print(main(x))
