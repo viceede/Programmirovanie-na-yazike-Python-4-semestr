e82d277239daf044646c10c58d0d9419d38dd7b6
viceede <d4rell1@yandex.ru>
2025-03-09 21:47:41 +0300

    1

--- Задача 6/6.py
+++ Задача 6/6.py
@@ -2,11 +2,11 @@ from math import floor
 
 
 def main(psi):
-    O = {floor(psi_i / 9) + psi_i ** 3 for psi_i in psi if -73 <= psi_i <= 42}
+    o = {floor(psi_i / 9) + psi_i ** 3 for psi_i in psi if -73 <= psi_i <= 42}
 
     Delta = {nu + abs(nu) for nu in range(-100, 101) if -52 <= nu <= -23}
 
-    N = {psi_i % 3 + floor(psi_i / 2) for psi_i in psi if psi_i >= 30 or psi_i <= -86}
+    n = {psi_i % 3 + floor(psi_i / 2) for psi_i in psi if psi_i >= 30 or psi_i <= -86}
 
     zeta = len(O)
     for o in O:
