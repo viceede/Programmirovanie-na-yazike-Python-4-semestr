6faf7a99b0b60666cb8dbb8716754f1d0a02d2d0
viceede <d4rell1@yandex.ru>
2025-03-11 22:26:56 +0300

    приведено к стандарту PEP8

--- 6.py
+++ 6.py
@@ -1,21 +1,18 @@
-import math
-
-
 def main(input_set):
-    # Определение множества N
-    N = {(psi % 3) + (psi // 2) for psi in input_set if (psi >= 30 or psi <= -86)}
+    # Определение множества n
+    n = {(psi % 3) + (psi // 2) for psi in input_set if (psi >= 30 or psi <= -86)}
 
-    # Определение множества Delta
-    Delta = {abs(nu) + nu for nu in N if (nu >= -23 or nu <= -52)}
+    # Определение множества delta
+    delta = {abs(nu) + nu for nu in n if (nu >= -23 or nu <= -52)}
 
-    # Определение множества O
-    O = {(psi // 9) + (psi ** 3) for psi in input_set if -73 <= psi <= 42}
+    # Определение множества o
+    o = {(psi // 9) + (psi ** 3) for psi in input_set if -73 <= psi <= 42}
 
     # Вычисление zeta
-    zeta = len(O)
-    for o in O:
-        for delta in Delta:
-            zeta += 6 * o + (delta // 5)
+    zeta = len(o)
+    for o_val in o:
+        for delta_val in delta:
+            zeta += 6 * o_val + (delta_val // 5)
 
     return zeta
 
