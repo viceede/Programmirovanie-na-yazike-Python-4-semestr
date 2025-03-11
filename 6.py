2fafbb448caaefb20d5c86c79f31a73da51e4a28
viceede <d4rell1@yandex.ru>
2025-03-11 22:03:42 +0300

    исправления в 6 задаче

--- 6.py
+++ 6.py
@@ -5,7 +5,8 @@ def calculate_n_set(psi):
     n_set = set()
     for psi_element in psi:
         if psi_element >= 30 or psi_element <= -86:
-            n_value = psi_element % 3 + math.ceil(psi_element / 2)
+            # Исправлено: используем целочисленное деление для корректного вычисления
+            n_value = (psi_element % 3) + ((psi_element + (1 if psi_element > 0 else 0)) // 2)
             n_set.add(n_value)
     return n_set
 
@@ -23,7 +24,8 @@ def calculate_o_set(psi):
     o_set = set()
     for psi_element in psi:
         if -73 <= psi_element <= 42:
-            o_value = math.floor(psi_element / 9) + psi_element ** 3
+            # Исправлено: используем целочисленное деление для корректного вычисления
+            o_value = (psi_element // 9) + (psi_element ** 3)
             o_set.add(o_value)
     return o_set
 
@@ -32,7 +34,8 @@ def calculate_zeta(o_set, delta_set):
     sum_part = 0
     for o in o_set:
         for delta in delta_set:
-            sum_part += delta * o + math.floor(delta / 5)
+            # Исправлено: используем целочисленное деление для корректного вычисления
+            sum_part += delta * o + (delta // 5)
     return len(o_set) + sum_part
 
 
@@ -42,3 +45,6 @@ def main(psi):
     o_set = calculate_o_set(psi)
     zeta = calculate_zeta(o_set, delta_set)
     return zeta
+
+print(main({-64, 65, 99, 36, -88, -21, -14, -78, -43, -2}))  # Ожидаемый результат: -5526162
+print(main({32, -92, 72, -52, -81, 82, -45, 84, 29}))       # Ожидаемый результат: -3142306
