b85b7d86d6b8b3c5af2c13da81bec8ff030805b9
viceede <d4rell1@yandex.ru>
2025-03-16 20:48:08 +0300

    привел к PEP8

--- 7.py
+++ 7.py
@@ -1,6 +1,8 @@
 def main(input_list):
     rules = {
-        ('HLSL', 2011): lambda x: 11 if x[3] == 'FORTH' and x[2] == 2003 and x[4] == 1977 else 1,
+        ('HLSL', 2011): lambda x: (
+            11 if x[3] == 'FORTH' and x[2] == 2003 and x[4] == 1977 else 1
+        ),
         ('HLSL', 1984): lambda x: 10 if x[2] == 2007 else 2,
         ('LUA', 2011): 0,
         ('YADA', 2019): 6,
@@ -20,10 +22,9 @@ def main(input_list):
         return rule
     return 0
 
-
 # Примеры использования:
 print(main(['HLSL', 2011, 2003, 'FORTH', 1977]))  # 11
 print(main(['HLSL', 2011, 2003, 'FORTH', 1978]))  # 1
-print(main(['LUA', 2011, 2003, 'ADA', 1978]))  # 0
-print(main(['HLSL', 1984, 2007, 'OX', 2019]))  # 10
-print(main(['HLSL', 1984, 2003, 'ADA', 1978]))  # 2
+print(main(['LUA', 2011, 2003, 'ADA', 1978]))     # 0
+print(main(['HLSL', 1984, 2007, 'OX', 2019]))     # 10
+print(main(['HLSL', 1984, 2003, 'ADA', 1978]))    # 2
